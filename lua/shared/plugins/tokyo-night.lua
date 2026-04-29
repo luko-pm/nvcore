b82ccf3f425d92df2d09e207ff229538c2c5b4e8
Lucas Perera <lucas.prm04@gmail.com>
2026-04-29 12:30:28 +0200

    cambiados highlights de código en md

--- lua/shared/plugins/tokyo-night.lua
+++ lua/shared/plugins/tokyo-night.lua
@@ -7,59 +7,29 @@ return{
         style = "night",
         transparent = true,
 
+
+        ---@param colors ColorScheme
+        on_colors = function(colors)
+            colors.bg_highlight = "#24283B"
+        end,
+
         ---@param hl tokyonight.Highlights
         ---@param c ColorScheme
-        on_hightlights = function(hl,c)
-            -- test
-            hl.CodeBlock = {
-                bg = "#16161e"
-            }
+        on_highlights = function(hl, c)
 
-            hl.RenderMarkdownCode = {
-                bg = "#16161e"
+            hl["@markup.raw.block.markdown"] = {
+                bg = c.bg_highlight
             }
 
             hl["@markup.raw.markdown_inline"] = {
-                bg = c.magenta2,
-                fg = "#7aa2f7"
-            }
-
-            hl.Comment = {
-                bg = "#7aa2f7"
+                bg = c.bg_highlight
             }
 
         end
     },
 
     config = function(_, opts)
-        --require("tokyonight").setup(opts) -- calling setup is optional
-        require('tokyonight').setup({
-        style = "night",
-        transparent = true,
-
-        ---@param hl tokyonight.Highlights
-        ---@param c ColorScheme
-        on_hightlights = function(hl,c)
-            -- test
-            hl.CodeBlock = {
-                bg = "#16161e"
-            }
-
-            hl.RenderMarkdownCode = {
-                bg = "#16161e"
-            }
-
-            hl["@markup.raw.markdown_inline"] = {
-                bg = c.magenta2,
-                fg = "#7aa2f7"
-            }
-
-            hl.Comment = {
-                bg = "#7aa2f7"
-            }
-
-        end
-        })
+        require("tokyonight").setup(opts) -- calling setup is optional
         vim.cmd [[colorscheme tokyonight]]
     end
 }
